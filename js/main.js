b4e1447de63660961b5bbe9d9a51ae720c403717
Oksana Khomko <homko3223@gmail.com>
2026-04-06 13:13:38 +0300

    Add new sections and style

--- js/main.js
+++ js/main.js
@@ -5,4 +5,40 @@ const swiper = new Swiper(".results__swiper", {
     nextEl: ".results__btn--next",
     prevEl: ".results__btn--prev",
   },
+
+  spaceBetween: 30,
 });
+
+new Accordion(".accordion-container", {
+  openOnInit: [0],
+});
+
+const phoneInput = document.querySelector(".registration__input--phone");
+if (phoneInput) {
+  const syncPhoneMask = () => {
+    const digits = phoneInput.value.replace(/\D/g, "");
+    if (!digits) {
+      phoneInput.value = document.activeElement === phoneInput ? "+" : "";
+    } else {
+      phoneInput.value = "+" + digits;
+    }
+  };
+
+  phoneInput.addEventListener("input", syncPhoneMask);
+
+  phoneInput.addEventListener("focus", () => {
+    if (phoneInput.value === "") {
+      phoneInput.value = "+";
+      requestAnimationFrame(() => {
+        const end = phoneInput.value.length;
+        phoneInput.setSelectionRange(end, end);
+      });
+    }
+  });
+
+  phoneInput.addEventListener("blur", () => {
+    if (phoneInput.value === "+") {
+      phoneInput.value = "";
+    }
+  });
+}
